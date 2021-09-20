4a918399661f327214536e1eaec3929a0585d248
Anthonylilo <ammen0203@gmail.com>
2021-09-20 10:51:11 -0400

    Parameters

--- configs/parameters.php
+++ configs/parameters.php
@@ -1,5 +1,5 @@
 <?php
-define("base_url", "http://localhost/micropar-pedidos/");
+define("base_url", "http://localhost/paginas/pedidospar/");
 define("controller_default", "IndexController");
 define("action_default", "index");
 ?>
